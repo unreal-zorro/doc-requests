8314494ab7bc77c904c86e7494880503e21cccf8
unreal-zorro <unrealzorro@gmail.com>
2024-04-11 21:00:26 +0300

    add auth-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import AuthPage from './components/auth-page/auth-page';
 import RequestPage from './components/request-page/request-page';
 
 function App() {
-  return <RequestPage />;
+  return (
+    <>
+      <AuthPage />;
+      <RequestPage />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/auth-page/auth-page.tsx
@@ -0,0 +1,5 @@
+function AuthPage() {
+  return <p>AuthPage</p>;
+}
+
+export default AuthPage;
